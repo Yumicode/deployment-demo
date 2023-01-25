d049cd417b9092943fce5acb27de7e8657f16b1e
Mayumi Park <mayumipark270@yahoo.com>
2023-01-25 11:58:24 -0700

    another update

--- solution/server.js
+++ solution/server.js
@@ -1,12 +1,15 @@
-const express = require('express')
+const express = require('express')                             //step 1
+
 const cors = require('cors')
-const app = express()
+
+const app = express()                                          //step 2
 
 app.use(cors())
-app.use(express.static(`${__dirname}/public`))
+
+app.use(express.static(`${__dirname}/public`))                  //step 4
 
 app.get('/api/cat', (req, res) => res.send('barb'))
 
-app.listen(4000, 
-    () => console.log(`server running on 4000`)
-)
+app.listen(4000,  () => {                                      //step 3 
+    console.log(`avengers assemble on 4000`)                  //step 3.1
+})
